019aadedf137f2a15bfb7e47aa7a2cceea293c04
Greg Gorlen <gsgorlen@gmail.com>
2020-11-15 16:55:46 -0800

    reenable code

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 set -eu -o pipefail
 sudo -n true
-#ln -sv ~/dotfiles/.bash_profile ~/.bash_profile
-#ln -sv ~/dotfiles/.vimrc ~/.vimrc
-#sudo apt update && sudo apt upgrade -y
+ln -sv ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sv ~/dotfiles/.vimrc ~/.vimrc
+sudo apt update && sudo apt upgrade -y
 
-#read -p "install nonessential packages? (y/n) " nonessential
-#
-#cat packages/essential.txt | xargs sudo apt -y install
-#[ $nonessential = "y" ] && cat packages/nonessential.txt | xargs sudo apt -y install
-#cat packages/python.txt | xargs python3 -m pip install
-#cat packages/node.txt | xargs npm i -g
-#
-#sudo apt autoremove -y
+read -p "install nonessential packages? (y/n) " nonessential
+
+cat packages/essential.txt | xargs sudo apt -y install
+[ $nonessential = "y" ] && cat packages/nonessential.txt | xargs sudo apt -y install
+cat packages/python.txt | xargs python3 -m pip install
+cat packages/node.txt | xargs npm i -g
+
+sudo apt autoremove -y
 source .bash_profile
