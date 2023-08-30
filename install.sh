d09616be3698a04b7765471794e3feb3edf8efc9
ggorlen <gsgorlen@gmail.com>
2023-08-30 12:17:34 -0700

    Overhaul install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+function prompt() {
+    read -p "$* [Y/n]: " yn
+    if [[ $yn = "y" || $yn = "Y" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 set -eu -o pipefail
 sudo -n true
 
-[ $# -eq 0 ] && { echo "no home directory provided"; exit 1; }
-
-home=$1
-
-ln -sv .bash_profile $home/.bash_profile
-ln -sv .bashrc $home/.bashrc
-ln -sv .vimrc $home/.vimrc
-
-read -p "install nonessential packages? (y/n) " nonessential
+ln -sv .bash_profile ~/.bash_profile
+ln -sv .bashrc ~/.bashrc
+ln -sv .vimrc ~/.vimrc
 
 sudo apt update && sudo apt upgrade -y
-cat packages/essential.txt | xargs sudo apt -y install
-[ $nonessential = "y" ] && cat packages/nonessential.txt | xargs sudo apt -y install
-cat packages/python.txt | xargs python3 -m pip install
-cat packages/node.txt | xargs npm i -g
 
-sudo apt autoremove -y
+if prompt "install essential packages?"; then
+    cat packages/essential.txt | xargs sudo apt -y install
+fi
+
+if prompt "install nonessential packages?"; then
+    cat packages/nonessential.txt | xargs sudo apt -y install
+fi
+
+#cat packages/python.txt | xargs python3 -m pip install
+#cat packages/node.txt | xargs npm i -g
+#cat packages/perl.txt | xargs cpan
+
+#sudo apt autoremove -y
 source .bash_profile
+
+# install nvm (update version)
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# install rvm or rbenv
+
+## YT-DLp https://github.com/yt-dlp/yt-dlp#installation
+
